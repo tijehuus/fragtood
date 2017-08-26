ef3ed7053c8583a9dc5b1b1c172356b4833addf3
tijehuus <31252273+tijehuus@users.noreply.github.com>
2017-08-26 12:27:15 +0000

    Update buddybuild_postclone.sh

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-#apt-get update && apt-get install -y --force-yes automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
+apt-get update && apt-get install -y --force-yes automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
